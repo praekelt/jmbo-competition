89cb9d2cadda3021e0347fc2538bff67be30ea40
Rizmari Versfeld <rizmari@praekelt.com>
2012-09-03 22:14:39 +0200

    lots of extra validation in admin since a competition can take a number of valid forms

--- competition/admin.py
+++ competition/admin.py
@@ -14,9 +14,9 @@ from competition.models import Competition, CompetitionEntry, \
 class CompetitionAnswerOptionAdminFormSet(BaseInlineFormSet):
     
     def clean(self):
-        super(CompetitionAnswerOptionAdminFormSet, self).clean()
+        cleaned_data = super(CompetitionAnswerOptionAdminFormSet, self).clean()
         if any(self.errors):
-            return    
+            return
         if self.instance:
             has_options = False
             for form in self.forms:
@@ -24,18 +24,40 @@ class CompetitionAnswerOptionAdminFormSet(BaseInlineFormSet):
                     has_options = True
                     break
             if has_options:
+                # check that answer type is multichoice if there are answer options
+                if self.instance.answer_type != 'multiple_choice_selection':
+                    raise forms.ValidationError(_("If you want to specify multiple choice answers, you need to set the answer type to 'Multiple choice selection'."))
+                # check that there is a question if answers are specified
                 if not self.instance.question:
-                    raise forms.ValidationError(_("You cannot have competition answers without a question."))
-                elif self.instance.correct_answer:
-                    raise forms.ValidationError(_("You must either provide a correct answer or answer options, not both."))
+                    raise forms.ValidationError(_("You cannot have answers without a question."))
+                # check that not both multichoice and a correct free text answer are specified
+                if self.instance.correct_answer:
+                    raise forms.ValidationError(_("You cannot provide both a correct free text answer and answer options. Only provide answers relevant to the selected answer type."))
+            else:
+                # multichoice requires answer options to be set
+                if self.instance.answer_type == 'multiple_choice_selection':
+                    raise forms.ValidationError(_("The answer type is set to 'Multiple choice selection' but there are no answer options."))
+        return cleaned_data
     
 
 class CompetitionAdminForm(ModelBaseAdminForm):
 
     def clean(self):
         cleaned_data = super(CompetitionAdminForm, self).clean()
-        if cleaned_data['correct_answer'] and not cleaned_data['question']:
-            raise forms.ValidationError(_("You cannot have competition answer without a question."))
+        at = cleaned_data['answer_type']
+        # check that file upload had max file size set
+        if at == 'file_upload' and not cleaned_data['max_file_size']:
+            raise forms.ValidationError(_("You need to specify a maximum file size."))
+        if at == 'free_text_input' or at == 'multiple_choice_selection':
+            # check that there is a question if an answer is required
+            if not cleaned_data['question']:
+                raise forms.ValidationError(_("You cannot have an answer without a question."))
+            # check that answer types match up
+            if cleaned_data['correct_answer'] and at == 'multiple_choice_selection':
+                raise forms.ValidationError(_("You specified a correct free text answer, but the answer type is not set to 'Free text input'."))
+        # check that an answer type has been specified if there is a question
+        if not at and cleaned_data['question']:
+            raise forms.ValidationError(_("You need to specify an answer type for the question."))
         return cleaned_data
 
 
@@ -65,7 +87,7 @@ class CompetitionAdmin(ModelBaseAdmin):
         self.fieldsets[0][1]['fields'] += one_liners
         
         question_fieldset = (('Competition question', {
-            'fields': ('question', 'question_blurb', 'answer_type', 'correct_answer',),
+            'fields': ('question', 'question_blurb', 'answer_type', 'correct_answer', 'max_file_size'),
             }), )
         for field in question_fieldset[0][1]['fields']:
             try:
